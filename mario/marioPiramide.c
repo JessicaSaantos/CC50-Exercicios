d4d1bd84135fa2af3d3e41561cd7c848f5e253e8
JessicaSaantos <jessicaferre998@gmail.com>
2022-08-03 19:32:31 -0300

    exercicio completo da piramide do mario

--- mario/marioPiramide.c
+++ mario/marioPiramide.c
@@ -2,20 +2,29 @@
 #include <cs50.h>
 
 int main (void) {
-    int height, row, column, space;
+    int height, row, column, space, gap;
     do {
       height = get_int ("Insira a altura da pirâmide: ");  
     } while(height < 1 || height > 8);
     
+    // loop externo imprime linhas 
     for (row = 0 ; row < height ; row ++){
         
         for (space = 0; space < height - row - 1; space ++){
             printf(" ");
         }
-        
+        // loop interno imprime colunas
         for(column = 0 ; column <= row ; column ++){
             printf("#");
         }
+        
+        for (gap = 0; gap <= height -1; gap = height){
+            printf("  ");
+        }
+        for(column = 0 ; column <= row ; column ++){
+            printf("#");
+        }
+        
         printf("\n");
     }
     
