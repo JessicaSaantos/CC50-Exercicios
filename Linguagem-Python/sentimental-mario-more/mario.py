df6c1ac3ccf6d18bd67c55ca8d788f8b165e4563
JessicaSaantos <jessicaferre998@gmail.com>
2022-10-08 23:29:24 -0300

    add resolução de mario more

---
+++ Linguagem-Python/sentimental-mario-more/mario.py
@@ -0,0 +1,17 @@
+# importando função get_int da biblioteca cs50
+from cs50 import get_int
+
+# Loop de verificação da altura
+while True:
+    n = get_int("Height: ")
+    if n > 0 and n < 9:
+        break
+
+# Usando for para estruturação da pirâmide
+for i in range(0, n, 1):
+    for j in range(0, n + i + 3, 1):
+        if (j == n or j == n + 1 or i + j < n - 1):
+            print(" ", end="")
+        else:
+            print("#", end="")
+    print()
